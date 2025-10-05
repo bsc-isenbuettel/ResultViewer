15cc3c66bb7b30494ecbcba9b02ef68f21a73fe9
AndrePes <mail@andre-peschyras.de>
2025-10-05 08:44:38 +0200

    Add shot of the day

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,12 @@ import ResultsTable from './components/ResultsTable';
 import { useEffect } from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Button from '@mui/material/Button';
 
 export const App = React.memo(() => {
   // Create state for entire data.
@@ -20,21 +26,33 @@ export const App = React.memo(() => {
     return arrays;
   }
 
-  function getLowestSplitValue(data: Record<string, { splits: number[] }[]>): number | null {
-  let lowest: number | null = null;
+  type SplitResult = {
+    value: number;
+    id: string;
+    firstName: string;
+    lastName: string;
+  } | null;
 
-  for (const entries of Object.values(data)) {
-    for (const entry of entries) {
-      for (const split of entry.splits) {
-        if (lowest === null || split < lowest) {
-          lowest = split;
+  function getLowestSplitInfo(data: { id: string; firstName: string; lastName: string; splits: number[] }[]): SplitResult {
+    let min = Infinity;
+    let result: SplitResult = null;
+
+    data.forEach(entry => {
+      entry.splits.forEach(split => {
+        if (split < min) {
+          min = split;
+          result = {
+            value: split,
+            id: entry.id,
+            firstName: entry.firstName,
+            lastName: entry.lastName,
+          };
         }
-      }
-    }
-  }
+      });
+    });
 
-  return lowest;
-}
+    return result;
+  }
 
   // Fetch data from backend. Ensure that backend is running!
   useEffect(() => {
@@ -81,15 +99,33 @@ export const App = React.memo(() => {
   }
 
   return (
-    <Container maxWidth="lg">
-      {data.map((item) => (
-        <React.Fragment key={item[0].date}>
-          <Typography variant="h6" gutterBottom>
-            {getDate(item[0].date)}
-          </Typography>
-          <ResultsTable rows={item} />
-        </React.Fragment>
-      ))}
+    <Container>
+      <Box marginBottom={5} sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+              BSC Result Viewer (v 0.1 - Alpha)
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+        {data.map((item) => {
+          const lowest = getLowestSplitInfo(item);
+          return (
+          <Box marginBottom={5}>
+          <React.Fragment key={item[0].date}>
+            <Typography variant="h4" gutterBottom>
+              {getDate(item[0].date)}
+            </Typography>
+            <Typography variant="h6" gutterBottom> Bester Teiler des Tages: {" "}{lowest
+            ? `${(lowest.value * 100000).toFixed(3)} (${lowest.firstName} ${lowest.lastName}, ID: ${lowest.id})`
+            : "N/A"}
+            </Typography>
+            <ResultsTable rows={item} />
+          </React.Fragment>
+          </Box>
+        );
+      })}
     </Container>
   ); 
 })
